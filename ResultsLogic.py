b2fadeb746e7805f59e2c2c8471a2e9e0d383444
Xavier4875 <youngerxavier@gmail.com>
2023-12-06 10:19:41 -0600

    Added files

--- ResultsLogic.py
+++ ResultsLogic.py
@@ -1,5 +1,7 @@
 from PyQt6.QtWidgets import *
 from ResultsScreen import *
+import re
+import csv
 
 
 class ResultsLogic(QMainWindow, Ui_ResultsScreen):
@@ -9,6 +11,55 @@ class ResultsLogic(QMainWindow, Ui_ResultsScreen):
         self.main_window = main_window
         self.results_home_button.clicked.connect(lambda: self.results_home())
 
+        self.clear_button.hide()
+        self.displayResults()
+
     def results_home(self):
+        with open('results.csv', 'w'):
+            pass
         self.hide()
         self.main_window.show()
+
+    def displayResults(self):
+        """
+
+        :return:
+        """
+        vicVotes = 0
+        nigilVotes = 0
+        donVotes = 0
+        with open('results.csv', 'r') as input_file:
+            for line in input_file:
+                if re.match(r'vic', line):
+                    vicVotes += 1
+                elif re.match(r'don', line):
+                    donVotes += 1
+                elif re.match(r'nigil', line):
+                    nigilVotes += 1
+                else:
+                    pass
+
+        all_votes = vicVotes + donVotes + nigilVotes
+        if all_votes > 0:
+            vic_pct = vicVotes / all_votes * 100
+            don_pct = donVotes / all_votes * 100
+            nigil_pct = nigilVotes / all_votes * 100
+
+            self.don_results_label.setText(f'{donVotes} votes\n{don_pct:.2f}%')
+            self.victor_results_label.setText(f'{vicVotes} votes\n{vic_pct:.2f}%')
+            self.nigil_results_label.setText(f'{nigilVotes} votes\n{nigil_pct:.2f}%')
+
+        else:
+            self.don_results_label.setText(f'0 votes\n0.00%')
+            self.victor_results_label.setText(f'0 votes\n0.00%')
+            self.nigil_results_label.setText(f'0 votes\n0.00%%')
+
+
+        if vicVotes > donVotes and vicVotes > nigilVotes:
+            self.winner_label.setText(f'The winner is Victor with {vicVotes}\nvotes! Congratulations!')
+        elif donVotes > vicVotes and donVotes > nigilVotes:
+            self.winner_label.setText(f'The winner is Don with {donVotes}\nvotes! Congratulations!')
+        elif nigilVotes > donVotes and nigilVotes > vicVotes:
+            self.winner_label.setText(f'The winner is Nigil with {nigilVotes}\nvotes! Congratulations!')
+        else:
+            self.winner_label.setText(f'Tie! We need more votes.')
--- VoteLogic.py
+++ VoteLogic.py
@@ -1,6 +1,7 @@
 from VoteScreen import *
 from PyQt6.QtWidgets import *
 from mainLogic import *
+import csv
 
 
 class VoteLogic(QMainWindow, Ui_VoteScreen):
@@ -8,11 +9,24 @@ class VoteLogic(QMainWindow, Ui_VoteScreen):
         super().__init__()
         self.setupUi(self)
         self.vote_home_button.clicked.connect(lambda: self.vote_home())
+        self.submit_button.clicked.connect(lambda: self.submit())
         self.main_window = main_window
 
     def vote_home(self):
         self.hide()
         self.main_window.show()
+
+    def submit(self):
+        with open('results.csv', 'a') as output_file:
+            csv_writer = csv.writer(output_file)
+
+            if self.don_vote_button.isChecked():
+                csv_writer.writerow(['don', 'I'])
+            if self.victor_vote_button.isChecked():
+                csv_writer.writerow(['vic', 'I'])
+            if self.nigil_vote_button.isChecked():
+                csv_writer.writerow(['nigil', 'I'])
+        self.vote_home()
 
 
 
